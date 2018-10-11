8e98bf02844d9f65c2d931d7f71f525efaf2c1d1
josephdlawson21 <josephdlawson21@gmail.com>
2018-10-10 22:54:42 -0400

    updated routes and added info to user json responses

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :schools
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json}  do
+   namespace :v1 do
+     resources :users
+     resources :schools
+   end
+ end
 end
