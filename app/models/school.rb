eb854b3285b47e4ddc21a9ca8d163a3a30e4e8a5
josephdlawson21 <josephdlawson21@gmail.com>
2018-10-10 23:05:01 -0400

    added validations to models

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,6 @@
 class School < ApplicationRecord
   has_many :users
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   belongs_to :school
+
+  validates :name, presence: true
 end
